244deacee3ad550b13b0517fdb22e79338feccd0
Germaine Pannell <me@germainep.com>
2020-05-03 19:39:03 -0400

    continued project initialization
    create knexfile.js and initial migration file also created files to being writing the server

--- knexfile.js
+++ knexfile.js
@@ -3,40 +3,18 @@
 module.exports = {
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3',
-    },
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
+      filename: './data/projects.db3',
     },
     migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
+      directory: './data/migrations/',
     },
     pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
+      afterCreate: ( conn, done ) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done) // turn on FK enforcement
+      },
     },
   },
 }
